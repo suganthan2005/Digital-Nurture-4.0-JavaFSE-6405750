fd721a90812156abe74a45c3b4114bfabef7d7fc
Suganthan Praba <suganthanpraba@Suganthans-MacBook-Air.local>
2025-06-19 11:31:16 +0530

    first commit

--- Design Patterns and Principles/FactoryMethodPatternExample/src/Main.java
+++ Design Patterns and Principles/FactoryMethodPatternExample/src/Main.java
@@ -1,12 +1,51 @@
+interface Document {
+    void open();
+}
 
-public class Main {
+class WordDocument implements Document {
+    public void open() {
+        System.out.println("Opening Word document");
+    }
+}
+
+class PdfDocument implements Document {
+    public void open() {
+        System.out.println("Opening PDF document");
+    }
+}
+
+class ExcelDocument implements Document {
+    public void open() {
+        System.out.println("Opening Excel document");
+    }
+}
+
+abstract class DocumentFactory {
+    abstract Document createDocument();
+}
+
+class WordFactory extends DocumentFactory {
+    Document createDocument() {
+        return new WordDocument();
+    }
+}
+
+class PdfFactory extends DocumentFactory {
+    Document createDocument() {
+        return new PdfDocument();
+    }
+}
+
+class ExcelFactory extends DocumentFactory {
+    Document createDocument() {
+        return new ExcelDocument();
+    }
+}
+
+class FactoryTest {
     public static void main(String[] args) {
-       
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            
-            System.out.println("i = " + i);
-        }
+        DocumentFactory factory = new PdfFactory();
+        Document doc = factory.createDocument();
+        doc.open();
     }
 }
