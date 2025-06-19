1059a39b9e98d57152fddfff346fc50e74e333d2
Suganthan Praba <suganthanpraba@Suganthans-MacBook-Air.local>
2025-06-19 11:06:14 +0530

    first commit

--- Design Patterns and Principles/SingletonPatternExample/src/Main.java
+++ Design Patterns and Principles/SingletonPatternExample/src/Main.java
@@ -1,4 +1,4 @@
-//LAZY INITIALIZATION
+
 class Logger{
     private static Logger instance;
     private Logger(){
@@ -11,8 +11,7 @@ class Logger{
         return instance;
     }
 }
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
         Logger instance1 = Logger.getInstance();
